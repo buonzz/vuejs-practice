21f77203984acf8a7a159d4017fc222937f56738
buonzz <buonzz@gmail.com>
2020-03-28 22:40:18 +0800

    layout for two columns

--- src/components/layouts/TwoColumnSideBar.vue
+++ src/components/layouts/TwoColumnSideBar.vue
@@ -1,5 +1,17 @@
 <template>
-    <div class="wrapper">
-        
+    <div class="grid-container">
+        <div class="left-column">This is the sidebar</div>
+        <div class="right-column"><slot/></div>
     </div>
 </template>
+<style scoped>
+    .grid-container{
+        width:100%;
+    }
+    .left-column {
+        width: 20%;
+    }
+    .right-column {
+        width: 80%;
+    }
+</style>
