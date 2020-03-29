7d38075225f10a2556549f4d1958da1a466f302a
buonzz <buonzz@gmail.com>
2020-03-29 15:31:23 +0800

    added dot

--- src/components/layouts/OneColumnSidebar.vue
+++ src/components/layouts/OneColumnSidebar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="grid-container">
         <slot/>
-        <h4>You are using the One Column Sidebar Layout</h4>
+        <h4> You are using the One Column Sidebar Layout</h4>
     </div>
 </template>
