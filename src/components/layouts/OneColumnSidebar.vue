8340f0396feab6025f06ec3ad3c2ea5ea30f5339
buonzz <buonzz@gmail.com>
2020-03-29 15:34:05 +0800

    re-added file

---
+++ src/components/layouts/OneColumnSidebar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="grid-container">
+        <div class="right-column"><slot/></div>
+        <h4>You are using the One Column Sidebar Layout</h4>
+    </div>
+</template>
+<style scoped>
+    .grid-container{
+        width:100%;
+    }
+</style>
